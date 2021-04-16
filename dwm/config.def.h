9669ceddb4018c2be73596f3d6678ad5662048e6
faizan-20 <faizan.20a@gmail.com>
2021-04-16 22:29:41 +0530

    minor changes

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -16,16 +16,19 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Hack Nerd Font:size=12" };
 static const char dmenufont[]       = "SF Pro Display:size=12";
+#include "/home/faizan/.cache/wal/colors-wal-dwm.h"
 static const char col_gray1[]       = "#282828";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#a89984";
 static const char col_gray4[]       = "#ebdbb2";
 static const char col_cyan[]        = "#458588";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-};
+static const char col_urgborder[]   = "#ff0000";
+//static const char *colors[][3]      = {
+  /*               fg         bg         border   */
+//	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+//	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+//	[SchemeUrg]  = { col_gray4, col_cyan,  col_urgborder  },
+//};
 
 /* tagging */
 static const char *tags[] = { "1-", "2-", "3-", "4-", "5-", "6", "7", "8", "9" };
@@ -45,7 +48,7 @@ static const Rule rules[] = {
 	{  "GParted",		NULL,			NULL,			0,			0,			1,			-1,			 0  },
 	{  "Timeshift-gtk",	NULL,			NULL,			0,			0,			1,			-1,			 0  },
 	{  "spotify",		NULL,			NULL,			5,			0,			0,			-1,			 0	},
-    {  "mailspring",    NULL,           NULL,           4,          0,          0,          -1,          0  },
+    {  "thunderbird",    NULL,           NULL,           4,          0,          0,          -1,          0  },
     {  "xdman-Main",    NULL,           NULL,           0,          0,          1,          -1,          0  },
 	{  NULL,			NULL,			"scratchpad",	0,          1,          1,          -1,			's' },
 	{  NULL,			NULL,			"htopcmd",		0,			1,			1,			-1,			'g' },
@@ -93,7 +96,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmen.sh", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
 static const char *scratchpadcmd[]  = {"s", "st", "-t", "scratchpad", NULL};
@@ -142,16 +145,19 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_e,      quit,           {0} },
+//	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {1} }, 
 	
 	/*Applications*/
 	{ 0,							XK_Print,  spawn,		   SHCMD("xfce4-screenshooter") },
 	{ MODKEY,						XK_w,	   spawn,		   SHCMD("firefox") },
-	{ MODKEY,						XK_e,	   spawn,		   SHCMD("thunar") },
+	{ MODKEY,						XK_e,	   spawn,		   SHCMD("pcmanfm") },
 	{ MODKEY|ShiftMask,				XK_x,	   spawn,		   SHCMD("i3lock-fancy") },
 	{ MODKEY|ShiftMask,				XK_s,	   spawn,		   SHCMD("LD_PRELOAD=/usr/local/lib/spotify-adblock.so spotify") },
 	{ MODKEY|ShiftMask,				XK_p,	   spawn,		   SHCMD("killall picom") },
 	{ MODKEY,						XK_p,	   spawn,		   SHCMD("picom --experimental-backends") },
-	{ MODKEY|ShiftMask,				XK_w,	   spawn,		   SHCMD("feh --bg-fill --randomize ~/Wallpapers/Walls") },
+//  { MODKEY|ShiftMask,				XK_w,	   spawn,		   SHCMD("feh --bg-fill --randomize ~/Wallpapers/Walls") },
+    { MODKEY,                       XK_x,      spawn,          SHCMD("./.local/bin/scripts/word-lookup.sh")},
+
 
 	/*Keyboard keys*/
   { 0, XF86XK_AudioMute,			spawn,		SHCMD("pamixer -t; dunstify -r 2 -t 750 \"VOL:$(pamixer --get-volume-human)\"") },
