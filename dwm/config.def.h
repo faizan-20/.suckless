21be28a12733a8d349c9d535e2ae99c41f829ba1
faizan-20 <faizan.20a@gmail.com>
2021-05-29 23:00:09 +0530

    minor changes

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -1,12 +1,12 @@
 /* See LICENSE file for copyright and license details. */
  
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 6;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 6;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 8;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 8;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 8;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 12;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 12;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 12;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
@@ -16,19 +16,19 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Hack Nerd Font:size=12" };
 static const char dmenufont[]       = "SF Pro Display:size=12";
-#include "/home/faizan/.cache/wal/colors-wal-dwm.h"
-static const char col_gray1[]       = "#282828";
-static const char col_gray2[]       = "#444444";
+//#include "/home/faizan/.cache/wal/colors-wal-dwm.h"
+static const char col_gray1[]       = "#000000";
+static const char col_gray2[]       = "#555555";
 static const char col_gray3[]       = "#a89984";
 static const char col_gray4[]       = "#ebdbb2";
-static const char col_cyan[]        = "#458588";
-static const char col_urgborder[]   = "#ff0000";
-//static const char *colors[][3]      = {
-///*               fg         bg         border   */
-//	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-//	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-//	[SchemeUrg]  = { col_gray4, col_cyan,  col_urgborder  },
-//};
+static const char col_cyan[]        = "#3f3f3f";
+static const char col_urgborder[]   = "#cc241d";
+static const char *colors[][3]      = {
+/*               fg         bg         border   */
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeUrg]  = { col_gray4, col_cyan,  col_urgborder  },
+};
 
 /* tagging */
 static const char *tags[] = { "1-", "2-", "3-", "4-", "5-", "6", "7", "8", "9" };
@@ -39,21 +39,22 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 
-	/* class			instance		title			tagsmask	iscentered	isfloating	monitor		scratchkey*/
-	{  "Gimp",			NULL,			NULL,			0,			0,			1,			-1,			 0  },
-	{  "firefox",		NULL,			NULL,			2,	        0,			0,			-1,			 0  },
-	{  "Brave-browser", NULL,			NULL,			2,		    0,          0,			-1,		     0 	},
-	{  "zoom",			NULL,			NULL,			1,			0,			1,			-1,			 0  },
-	{  "qBittorrent",	NULL,			NULL,			0,			0,			1,			-1,			 0  },
-	{  "GParted",		NULL,			NULL,			0,			0,			1,			-1,			 0  },
-	{  "Timeshift-gtk",	NULL,			NULL,			0,			0,			1,			-1,			 0  },
-	{  "spotify",		NULL,			NULL,			5,			0,			0,			-1,			 0	},
-    {  "thunderbird",   NULL,           NULL,           4,          0,          0,          -1,          0  },
-    {  "xdman-Main",    NULL,           NULL,           0,          0,          1,          -1,          0  },
-	{  NULL,			NULL,			"scratchpad",	0,          1,          1,          -1,			's' },
-	{  NULL,			NULL,			"htopcmd",		0,			1,			1,			-1,			'g' },
-	{  NULL,			NULL,	        "bccmd",        0,          1,          1,          -1,         'c' },
-	{  NULL,	        NULL,			"pulsemixercmd",0,          1,          1,          -1,         'p' },
+	/* class			    instance		title			tagsmask	iscentered	isfloating	monitor		scratchkey*/
+	{  "Gimp",		    	NULL,			NULL,			0,			0,			1,			-1,			 0  },
+	{  "firefox",		    NULL,			NULL,			2,	        0,			0,			-1,			 0  },
+	{  "Brave-browser",     NULL,			NULL,			2,		    0,          0,			-1,		     0 	},
+	{  "Chromium",          NULL,			NULL,			2,		    0,          0,			-1,		     0 	},
+	{  "Navigator",         NULL,			NULL,			2,		    0,          0,			-1,		     0 	},
+	{  "zoom",			    NULL,			NULL,			1,			0,			1,			-1,			 0  },
+	{  "qBittorrent",	    NULL,			NULL,			0,			0,			1,			-1,			 0  },
+	{  "GParted",		    NULL,			NULL,			0,			0,			1,			-1,			 0  },
+	{  "Timeshift-gtk",	    NULL,			NULL,			0,			0,			1,			-1,			 0  },
+    {  "thunderbird",       NULL,           NULL,           4,          0,          0,          -1,          0  },
+    {  "Blueman-manager",   NULL,           NULL,           0,          1,          1,          -1,          0  },
+	{  NULL,			    NULL,			"scratchpad",	0,          1,          1,          -1,			's' },
+	{  NULL,			    NULL,			"htopcmd",		0,			1,			1,			-1,			'g' },
+	{  NULL,			    NULL,	        "bccmd",        0,          1,          1,          -1,         'c' },
+	{  NULL,	            NULL,			"pulsemixercmd",0,          1,          1,          -1,         'p' },
 };
 
 /* layout(s) */
@@ -96,10 +97,10 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmen.sh", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *termcmd[]  = { "st", NULL };
 
-static const char *scratchpadcmd[]  = {"s", "alacritty", "-t", "scratchpad", NULL};
+static const char *scratchpadcmd[]  = {"s", "st", "-t", "scratchpad", NULL};
 static const char *htopcmd[]		= {"g", "st", "-t", "htopcmd", "-e", "gotop", NULL}; 
 static const char *bccmd[]			= {"c", "st", "-t", "bccmd", "-g", "40x20", "-e", "bc", "-lq", NULL};
 static const char *pulsemixercmd[]	= {"p", "st", "-t", "pulsemixercmd", "-e", "pulsemixer", NULL}; 
@@ -148,13 +149,14 @@ static Key keys[] = {
 	
 	/*Applications*/
 	{ 0,							XK_Print,  spawn,		   SHCMD("xfce4-screenshooter") },
-	{ MODKEY,						XK_w,	   spawn,		   SHCMD("firefox") },
+	{ MODKEY|ShiftMask,				XK_w,	   spawn,		   SHCMD("firefox") },
+    { MODKEY,	        			XK_w,	   spawn,		   SHCMD("chromium") },
 	{ MODKEY,						XK_e,	   spawn,		   SHCMD("pcmanfm") },
+    { MODKEY|ShiftMask,             XK_b,      spawn,          SHCMD("blueman-manager") },
 	{ MODKEY|ShiftMask,				XK_x,	   spawn,		   SHCMD("i3lock-fancy") },
 	{ MODKEY|ShiftMask,				XK_s,	   spawn,		   SHCMD("LD_PRELOAD=/usr/local/lib/spotify-adblock.so spotify") },
 	{ MODKEY|ShiftMask,				XK_p,	   spawn,		   SHCMD("killall picom") },
 	{ MODKEY,						XK_p,	   spawn,		   SHCMD("picom --experimental-backends") },
-//  { MODKEY|ShiftMask,				XK_w,	   spawn,		   SHCMD("feh --bg-fill --randomize ~/Wallpapers/Walls") },
     { MODKEY,                       XK_x,      spawn,          SHCMD("./.local/bin/scripts/word-lookup.sh")},
 
 
